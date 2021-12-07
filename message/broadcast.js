cbf0aaf6c82a2a0acb88e1b702ce0522b2ba1acf
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-07 16:54:53 +0700

    Update broadcast.js

--- message/broadcast.js
+++ message/broadcast.js
@@ -1,3 +1,1 @@
-const { downloadContentFromMessage } = require('@adiwajshing/baileys-md')
-const { WAConnection, MessageType, Presence, Mimetype, relayWAMessage, prepareMessageFromContent, GroupSettingChange } = require('@adiwajshing/baileys')
-const fs = require('fs-extra')
+
