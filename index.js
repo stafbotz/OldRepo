8f9139468b23775001e478d4a78db6f50df470c9
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-08 22:00:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReaso
 const { state, saveState } = useSingleFileAuthState('./session.json')
 const pino = require('pino')
 const { color, bgcolor } = require('./lib/color')
+const { clientLog } = require('.lib/functions')
 const fs = require('fs-extra')
 
 async function start() {
+    clientLog('warn', 'make socket a wa web')
     const client = makeWASocket({ 
        printQRInTerminal: true, 
        logger: pino({ level: 'fatal' }),
        auth: state
     })
-    console.log('client', color('INFO!', 'green'), 'start connection to wa web')
+    clientLog('info', 'start connection to wa web')
     client.ev.on('messages.upsert', async (mek) => {
        if (!mek.messages) return
        const msg = mek.messages[0]
@@ -19,17 +21,17 @@ async function start() {
     client.ev.on('connection.update', (update) => {
        const { connection, lastDisconnect } = update
        if (connection === 'close') {
-         console.log('connection closed, try to restart')
+         clientLog('warn', 'connection closed, try to restart')
          lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
          ? start()
-         : console.log('client', color('INFO!', 'green'), 'whatsapp web is logged out')
+         : clientLog('err', 'whatsapp web is logged out')
        }
        if (connection === 'connecting') {
-         console.log('client', color('INFO!', 'green'),'connected to wa web')
-         console.log('client', color('INFO!', 'green'), 'start connection to client')
+         clientLog('info', 'connected to wa web')
+         clientLog('info', 'start connection to client')
        }
        if (connection === 'open') {
-         console.log('client', color('INFO!', 'green'), 'opened connection')
+         clientLog('info', 'opened connection')
          require('./message/broadcast.js')(client)
        }
     })
