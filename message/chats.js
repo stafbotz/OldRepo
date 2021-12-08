d9bf237c3994efb29de9b1a863d20417792eaa46
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-08 21:20:09 +0700

    Update chats.js

--- message/chats.js
+++ message/chats.js
@@ -1,10 +1,10 @@
 const { downloadContentFromMessage } = require('@adiwajshing/baileys-md')
-const { h2k, getBuffer, randomBytes, generateMessageID, getGroupAdmins, getRandom } = require('./lib/function')
+const { h2k, getBuffer, randomBytes, generateMessageID, getGroupAdmins, getRandom } = require('../lib/function')
 const fs = require('fs-extra')
 const moment = require('moment-timezone')
 const { exec, spawn } = require("child_process")
 const axios = require('axios')
-const { color, bgcolor } = require('./lib/color')
+const { color, bgcolor } = require('../lib/color')
 
 moment.tz.setDefault('Asia/Jakarta').locale('id')
 module.exports = async(client, msg, mek) => {
