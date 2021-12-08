bf744312bd5bccb9f6fe7416ba42fed7649ecc57
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-08 13:58:37 +0700

    Update broadcast.js

--- message/broadcast.js
+++ message/broadcast.js
@@ -1,1 +1,12 @@
+const { prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
+const fs = require('fs-extra')
+const { color, bgcolor } = require('./lib/color')
+const moment = require('moment-timezone')
 
+moment.tz.setDefault('Asia/Jakarta').locale('id')
+module.exports = async(client) => {
+    try {
+    } catch (err) {
+        console.log('unexpected error: ' + err)
+    }
+}
