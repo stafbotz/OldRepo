2c25e78540eb8da776e9db8fe7c3147723258c4a
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-09 16:29:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ async function start() {
     clientLog('warn', 'make socket a wa web')
     const client = makeWASocket({ 
        printQRInTerminal: true, 
-       logger: pino({ level: 'fatal' }),
+       logger: pino({ level: 'debug' }),
        auth: state
     })
     clientLog('info', 'start connection to wa web')
     client.ev.on('messages.upsert', async (mek) => {
-       if (!mek.messages) return
-       const msg = mek.messages[0]
        try {
+             if (!mek.messages) return
+             const msg = mek.messages[0]
+             const content = JSON.stringify(msg.message)
              const type = Object.keys(msg.message)[0]
              const body = (type === 'conversation' && msg.message.conversation.startsWith('')) ? msg.message.conversation : (type == 'imageMessage') && msg.message.imageMessage.caption.startsWith('') ? msg.message.imageMessage.caption : (type == 'documentMessage') && msg.message.documentMessage.caption.startsWith('') ? msg.message.documentMessage.caption : (type == 'videoMessage') && msg.message.videoMessage.caption.startsWith('') ? msg.message.videoMessage.caption : (type == 'extendedTextMessage') && msg.message.extendedTextMessage.text.startsWith('') ? msg.message.extendedTextMessage.text : (type == 'buttonsResponseMessage' && msg.message.buttonsResponseMessage.selectedButtonId.startsWith('')) ? msg.message.buttonsResponseMessage.selectedButtonId : (type == 'templateButtonReplyMessage') && msg.message.templateButtonReplyMessage.selectedId.startsWith('') ? msg.message.templateButtonReplyMessage.selectedId.startsWith('') : ""
              const budy = (type === 'conversation') ? msg.message.conversation : (type === 'extendedTextMessage') ? msg.message.extendedTextMessage.text : ''	
