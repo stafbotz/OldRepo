c7f000b08a386d73707648d0e2f07a9368b8ee71
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-09 16:22:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason, AnyMessageContent, delay, useSingleFileAuthState } = require('@adiwajshing/baileys-md')
+// Module
+const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason, AnyMessageContent, delay, useSingleFileAuthState, downloadContentFromMessage } = require('@adiwajshing/baileys-md')
 const { state, saveState } = useSingleFileAuthState('./session.json')
 const pino = require('pino')
 const { color, bgcolor } = require('./lib/color')
-const { clientLog } = require('./lib/functions')
+const { h2k, getBuffer, randomBytes, generateMessageID, getGroupAdmins, getRandom, clientLog } = require('./lib/functions')
 const fs = require('fs-extra')
+const axios = require('axios')
+const moment = require('moment-timezone')
+const { exec, spawn } = require('child_process')
+
+moment.tz.setDefault('Asia/Jakarta').locale('id')
+
+// Database
+const antilink = JSON.parse(fs.readFileSync('./database/antilink.json'))
+const registered = JSON.parse(fs.readFileSync('./database/registered.json'))
 
 async function start() {
     clientLog('warn', 'make socket a wa web')
@@ -16,7 +26,18 @@ async function start() {
     client.ev.on('messages.upsert', async (mek) => {
        if (!mek.messages) return
        const msg = mek.messages[0]
-       require('./message/chats.js')(client, msg, mek)
+       try {
+             const type = Object.keys(msg.message)[0]
+             const body = (type === 'conversation' && msg.message.conversation.startsWith('')) ? msg.message.conversation : (type == 'imageMessage') && msg.message.imageMessage.caption.startsWith('') ? msg.message.imageMessage.caption : (type == 'documentMessage') && msg.message.documentMessage.caption.startsWith('') ? msg.message.documentMessage.caption : (type == 'videoMessage') && msg.message.videoMessage.caption.startsWith('') ? msg.message.videoMessage.caption : (type == 'extendedTextMessage') && msg.message.extendedTextMessage.text.startsWith('') ? msg.message.extendedTextMessage.text : (type == 'buttonsResponseMessage' && msg.message.buttonsResponseMessage.selectedButtonId.startsWith('')) ? msg.message.buttonsResponseMessage.selectedButtonId : (type == 'templateButtonReplyMessage') && msg.message.templateButtonReplyMessage.selectedId.startsWith('') ? msg.message.templateButtonReplyMessage.selectedId.startsWith('') : ""
+             const budy = (type === 'conversation') ? msg.message.conversation : (type === 'extendedTextMessage') ? msg.message.extendedTextMessage.text : ''	
+	     const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
+             const args = body.trim().split(/ +/).slice(1)
+             const q = args.join(' ')
+             const pushname = (msg.pushName != '' || msg.pushName != undefined) ? msg.pushName : undefined
+             const isCmd = body.startsWith('')
+       } catch (err) {
+          console.log('unexpected error: ' + err)
+      }
     })
     client.ev.on('connection.update', (update) => {
        const { connection, lastDisconnect } = update
@@ -32,7 +53,6 @@ async function start() {
        }
        if (connection === 'open') {
          clientLog('info', 'opened connection')
-         require('./message/broadcast.js')(client)
        }
     })
   client.ev.on('creds.update', () => saveState)
