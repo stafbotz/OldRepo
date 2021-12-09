6ce9be6b66215cedb579ccb8af7e7aad4cd28ec7
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-10 06:13:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ async function start() {
              const isQuotedVideo = isQuotedMsg ? content.includes('videoMessage') ? true : false : false
              const isQuotedSticker = isQuotedMsg ? content.includes('stickerMessage') ? true : false : false
              
-             const mentionTag = type == 'extendedTextMessage' && msg.message.extendedTextMessage.contextInfo != null ? msg.message.extendedTextMessage.contextInfo.mentionedJid : []
-             const mentionReply = type == 'extendedTextMessage' && msg.message.extendedTextMessage.contextInfo != null ? msg.message.extendedTextMessage.contextInfo.participant || '' : ''
+             const mentionTag = type == 'extendedTextMessage' && msg.message.contextInfo != null ? msg.message.contextInfo.mentionedJid : []
+             const mentionReply = type == 'extendedTextMessage' && msg.message.contextInfo != null ? msg.message.contextInfo.participant || '' : ''
              const mention = typeof(mentionTag) == 'string' ? [mentionTag] : mentionTag
              mention != undefined ? mention.push(mentionReply) : []
              const mentionUser = mention != undefined ? mention.filter(n => n) : []
