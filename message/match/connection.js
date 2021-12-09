5da32f079d43b117f9a69681baa90d80301e57ab
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-09 16:23:08 +0700

    Delete message directory

--- message/match/connection.js
+++
@@ -1,3 +0,0 @@
-const { downloadContentFromMessage } = require('@adiwajshing/baileys-md')
-const { WAConnection, MessageType, Presence, Mimetype, relayWAMessage, prepareMessageFromContent, GroupSettingChange } = require('@adiwajshing/baileys')
-const fs = require('fs-extra')
