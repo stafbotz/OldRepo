89808f233bafb7b12fce70276d97a504fa971b6e
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-10 19:06:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -72,12 +72,6 @@ async function start() {
              const isQuotedVideo = isQuotedMsg ? content.includes('videoMessage') ? true : false : false
              const isQuotedSticker = isQuotedMsg ? content.includes('stickerMessage') ? true : false : false
              
-             const mentionTag = type == 'extendedTextMessage' && msg.message.extendedTextMessage.contextInfo == null ? [] : msg.message.extendedTextMessage.contextInfo.mentionedJid != undefined ? msg.message.extendedTextMessage.contextInfo.mentionedJid : []
-             const mentionReply = type == "extendedTextMessage" && msg.message.extendedTextMessage.contextInfo == null ? '' : msg.message.extendedTextMessage.contextInfo.participant != undefined ? msg.message.extendedTextMessage.contextInfo.participant : ''
-             const mention = typeof(mentionTag) == 'string' ? [mentionTag] : mentionTag
-             mention != undefined ? mention.push(mentionReply) : []
-             const mentionUser = mention != undefined ? mention.filter(n => n) : []
-             
              const reply = (text, mentions) => {
                  return client.sendMessage(from, { text: text, mentions: mentions ? mentions : [] }, { quoted: msg })
              }
@@ -140,10 +134,6 @@ async function start() {
                  break
                  case 'msg' :
                      client.sendMessage(from, { text : content }, { quoted: msg })
-                 break
-                 case 'getmentioned' :
-                     if (!q) return reply('Tag Target!')
-                     client.sendMessage(from, { text : mentionUser }, { quoted: mek })
                  break
                  default:
              }	
