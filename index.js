a473c904ddddabdd58b22174ed7e8333edc9bdda
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-12 04:29:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -351,7 +351,8 @@ async function start() {
                        .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
                        .toFormat('webp')
                        .save(ran)  
-                  break
+                      }
+                 break
                  case 'toimg':
                      if (!isQuotedSticker) return reply('Reply Stiker!')
                      reply('Memproses')
