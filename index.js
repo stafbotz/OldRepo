8020ec69deff933e2263466d7a490b76b03fa75c
ClownwithCloud <90103953+stafbotz@users.noreply.github.com>
2021-12-12 13:58:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -357,7 +357,7 @@ async function start() {
                  case 'toimg':
                      if (!isQuotedSticker) return reply('Reply Stiker!')
                      reply('Memproses')
-                     var media = await downloadContentFromMessage(msg.message.extendedTextMessage.contextInfo.quotedMessage, 'stickerMessage')
+                     var media = await downloadContentFromMessage(msg.message.extendedTextMessage.contextInfo.quotedMessage.stickerMessage, 'stickerMessage')
                      var ran = await getRandom('.png')
                      exec(`ffmpeg -i ${media} ${ran}`, (err) => {
                           fs.unlinkSync(media)
